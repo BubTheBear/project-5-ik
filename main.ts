2ae254acf617a1071f748efd438b9a4d101fa40f
BubTheBear <97460887+BubTheBear@users.noreply.github.com>
2022-02-17 09:29:13 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-let numofletters = game.askForNumber("")
+let numofletters = game.askForNumber("Number of letters in your name Chaser?", 10)
+if (true) {
+	
+} else if (false) {
+	
+}
+let Name = game.askForString("What is your name Chaser?", numofletters)
+let numoflettersinrunnername = game.askForNumber("Number of letters in your name Runner?", 10)
+if (true) {
+	
+} else if (false) {
+	
+}
+let Name_of_Runner = game.askForString("What is your name Runner?", numoflettersinrunnername)
